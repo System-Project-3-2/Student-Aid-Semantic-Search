4b21b0a73c5a10107ab4c6cbd6b30d38942ed5cc
niloy <chowdhuryniloy819@gmail.com>
2025-12-25 22:52:05 +0600

    designed the controller for file uploading feature

---
+++ backend/controllers/materialController.js
@@ -0,0 +1,44 @@
+import Material from "../models/Material.js";
+import extractPdfText from "../utils/pdfParser.js";
+import extractDocText from "../utils/docParser.js";
+import uploadToCloudinary from "../utils/cloudinaryUpload.js";
+
+import path from "path";
+
+export const uploadMaterial = async (req, res) => {
+  try {
+    const { title, course, type } = req.body;
+    const file = req.file;
+
+    if (!req.file) {
+      return res.status(400).json({ message: "File missing" });
+    }
+
+    let extractedText = "";
+    const ext = path.extname(file.originalname).toLowerCase();
+
+    if (ext === ".pdf") {
+      extractedText = await extractPdfText(file.path);
+    } else if (ext === ".doc" || ext === ".docx") {
+      extractedText = await extractDocText(file.path);
+    } else {
+      return res.status(400).json({ message: "Unsupported file type" });
+    }
+
+    const fileUrl = await uploadToCloudinary(file.path);
+
+    const material={
+      title,
+      course,
+      type,
+      fileUrl,
+      extractedText,
+    };
+
+    const newMaterial = await Material.create(material);
+
+    res.status(201).json(newMaterial);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
