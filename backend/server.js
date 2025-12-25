11a2d274582ac17261fd8cd2722f230090c9ae12
niloy <chowdhuryniloy819@gmail.com>
2025-12-25 15:02:36 +0600

    build the basic server structure and connected with  mongodb server

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+
+const app = express();
+
+dotenv.config();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/auth', authRoutes);
+app.use('/api/materials', materialRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
