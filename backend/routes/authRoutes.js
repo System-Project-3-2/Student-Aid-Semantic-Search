c3902de4b7be26cd412536fe6aae86149f3e6d65
niloy <chowdhuryniloy819@gmail.com>
2025-12-29 23:07:48 +0600

    included the otp-authentication endpoint

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { registerUser, loginUser } from '../controllers/authController.js';
+import { verifyOtp } from '../controllers/otpController.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/register', registerUser);
 authRouter.post('/login', loginUser);
+authRouter.post('/verify-otp', verifyOtp);
 
 export default authRouter;
