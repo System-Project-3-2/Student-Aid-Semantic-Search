4eb16f596f5910ff51daf922d925330b362747a7
niloy <chowdhuryniloy819@gmail.com>
2026-01-04 22:24:26 +0600

    added the deletematerial route for teacher only role

--- backend/routes/materialRoutes.js
+++ backend/routes/materialRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
 import upload from "../middleware/uploadMiddleware.js";
-import { uploadMaterial } from "../controllers/materialController.js";
+import {
+  uploadMaterial,
+  deleteMaterial,
+} from "../controllers/materialController.js";
 import { protect } from "../middleware/authMiddleware.js";
 import { authorize } from "../middleware/roleMiddleware.js";
 
 const router = express.Router();
 
 router.post("/upload", protect, upload.single("file"), uploadMaterial);
+router.delete("/:id", protect, authorize("teacher"), deleteMaterial);
+
 export default router;
