a7bcca627f42bc0054b37f50a0eb51f8e2e3017c
niloy <chowdhuryniloy819@gmail.com>
2026-01-13 18:44:09 +0600

    fixed an issue while uplaoding a pdf

--- backend/utils/pdfParser.js
+++ backend/utils/pdfParser.js
@@ -1,10 +1,20 @@
 import fs from "fs";
-import * as pdf from "pdf-parse";
+import { getDocument } from "pdfjs-dist/legacy/build/pdf.mjs";
 
 const extractPdfText = async (filePath) => {
-  const dataBuffer = fs.readFileSync(filePath);
-  const data = await pdf.default(dataBuffer);
-  return data.text;
+  const dataBuffer = new Uint8Array(fs.readFileSync(filePath));
+  const pdf = await getDocument({ data: dataBuffer }).promise;
+  
+  let fullText = "";
+  
+  for (let i = 1; i <= pdf.numPages; i++) {
+    const page = await pdf.getPage(i);
+    const textContent = await page.getTextContent();
+    const pageText = textContent.items.map((item) => item.str).join(" ");
+    fullText += pageText + "\n";
+  }
+  
+  return fullText;
 };
 
 export default extractPdfText;
