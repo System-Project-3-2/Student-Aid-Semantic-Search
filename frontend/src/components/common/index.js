de2188f53f429708b9b33ac69727706aeb12e157
jony2511 <141722597+jony2511@users.noreply.github.com>
2026-01-21 18:49:01 +0600

    common index is now added

---
+++ frontend/src/components/common/index.js
@@ -0,0 +1,13 @@
+/**
+ * Common Components Index
+ * Export all common components from a single point
+ */
+export { default as Navbar } from './Navbar';
+export { default as Sidebar, drawerWidth } from './Sidebar';
+export { default as LoadingSpinner } from './LoadingSpinner';
+export { default as ProtectedRoute } from './ProtectedRoute';
+export { default as StatCard } from './StatCard';
+export { default as PageHeader } from './PageHeader';
+export { default as EmptyState } from './EmptyState';
+export { default as FeedbackCard } from './FeedbackCard';
+export { default as MaterialCard } from './MaterialCard';
